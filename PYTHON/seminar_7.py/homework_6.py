52831f5b5c23fb15e8f230589b60fd629f0b3908
EBalditsyn <kat.balditsyna@yandex.ru>
2023-08-23 16:52:33 +0300

    homework_6

---
+++ PYTHON/seminar_7.py/homework_6.py
@@ -0,0 +1,14 @@
+# a1 = 7
+# d = 2
+# n = 5
+# for i in range(n):
+#     print(a1 + i * d, end=" ")
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+list_2 = []
+max = 10
+min = 6
+
+for i in range(len(list_1)):
+    if min <= list_1[i] <= max:
+        print(i, end=" ")
